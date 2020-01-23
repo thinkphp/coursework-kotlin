06933d3ba3a2c3704e9750a38fc70a1755d36a8a
Adrian Statescu <mergesortv@gmail.com>
2020-01-23 11:47:31 +0200

    Create char-string.kt

---
+++ char-string.kt
@@ -0,0 +1,15 @@
+fun main(argv: Array<String>) {
+	
+	var c: Char
+
+	val sentence: String = "I see the life almost one long university that I never had, everyday I am learning something new." 
+
+	for(character in sentence) {
+
+        //value of character assigned to c without error.
+	    c = character
+	    print(c + " ")
+	}
+
+	println("")
+}
